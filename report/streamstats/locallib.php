8804d53e7a409c39bf7cfccd7d7e84d326eec541
Kieran Briggs <kbriggs@chartered.college>
2019-05-15 16:09:24 +0100

    Added new function and sql to locallib
    function get_workstream_options

--- report/streamstats/locallib.php
+++ report/streamstats/locallib.php
@@ -3,8 +3,32 @@
 function get_workstream_categories() {
     global $DB;
 
-    $sql = 'SELECT cc.name, cc.idnumber from {course_categories} WHERE cc.visible = 1';
-    $rs = $DB->get_record_sql($sql);
+    $sql = 'SELECT cc.name, cc.id from {course_categories} as cc WHERE cc.visible = 1';
+    $rs = $DB->get_records_sql($sql);
 
     return $rs;
+}
+
+function get_workstream_options() {
+
+
+    $categories = get_workstream_categories();
+    foreach($categories as $cat) {
+        $options[$cat->id] = $cat->name;
+    }
+
+    $mform = $this->_mform;
+
+    $mform->add_element('select', 'categories', get_string('catoptions', 'report_streamstats'), $options);
+
+
+
+    return $mform;
+
+}
+
+function forum_statistics($category = 1) {
+    global $DB;
+
+    $sql = ""
 }
