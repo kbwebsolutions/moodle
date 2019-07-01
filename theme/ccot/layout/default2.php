70a62a839337a1926a50bb8cd586d8484a813a40
Kieran Briggs <wheelz24@gmail.com>
2019-07-01 18:42:58 +0100

    Revert "Change back button in activites"
    This reverts commit 8abd09a94f85a7c137b5a4c1d721f0030f2b8114.

--- theme/ccot/layout/default2.php
+++ theme/ccot/layout/default2.php
@@ -125,7 +125,7 @@ if ($PAGE->pagelayout === 'frontpage' && $PAGE->pagetype === 'site-index') {
 }
 
 //echo $OUTPUT->activity_navigation();
-echo $OUTPUT->backToSection($mastimage);
+echo "<hr /><p> <a class='btn btn-secondary'  href='".$CFG->wwwroot."/course/view.php?id=".$COURSE->id."'>Back to module homepage</a></p>";
 echo $OUTPUT->course_content_footer();
 
 if (stripos($PAGE->bodyclasses, 'format-singleactivity') !== false ) {
