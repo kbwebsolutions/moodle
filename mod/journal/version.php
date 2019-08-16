765ad04e8a78b6660b760ae7c4984b010cc88e5a
Kieran Briggs <kbriggs@kbwebsolutions.co.uk>
2019-08-16 16:13:00 +0100

    Upgrade to Journal Activity 2.0

--- mod/journal/version.php
+++ mod/journal/version.php
@@ -20,8 +20,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_journal';
-$plugin->version  = 2018012200;
-$plugin->requires = 2017111300;  /* Moodle 3.4 */
-$plugin->release = '34.0 (Build: 2017121100)';
+$plugin->version  = 2019061600;
+$plugin->requires = 2013111800;  /* Moodle 2.6 */
+$plugin->release = '2.0.0 (Build: 2019061600)';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->cron     = 60;
