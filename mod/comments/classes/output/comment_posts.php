5069aa81f6a40fdd9d4d095676bc9443815a0129
Kieran Briggs <kbriggs@kbwebsolutions.co.uk>
2019-08-16 14:09:55 +0100

    Create the export for template and comment_posts class

---
+++ mod/comments/classes/output/comment_posts.php
@@ -0,0 +1,52 @@
+<?php
+
+
+namespace mod_comments\output;
+
+
+use renderer_base;
+use renderable;
+use templatable;
+
+class comment_posts implements renderable, templatable {
+
+    /**
+     * @var array
+     */
+    protected $comments;
+
+    /**
+     * comments_posts constructor.
+     * @param array $posts
+     */
+    public function __construct(array $posts = []) {
+        global $DB;
+
+        $this->comments = [];
+
+        foreach ($posts as $key => $message) {
+            $this->comments[] = $message;
+        }
+    }
+
+
+    public function export_for_template(renderer_base $output)
+    {
+        global $DB;
+
+        $data = ['messages' => []];
+        foreach ($this->comments as $key => $comment) {
+            $data['messages'][$key] = $comment;
+            $user = $DB->get_record('user', array('id' => $comment->userid));
+            $userpix = $output->user_picture($user);
+            $data['messages'][$key]->userpix = $userpix;
+
+
+
+
+        }
+
+        return $data;
+    }
+
+}
