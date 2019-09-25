8c7db53cf4be754d4e2ffcc44309a92fc432e9ba
Kieran Briggs <kbriggs@kbwebsolutions.co.uk>
2019-09-25 12:36:54 +0100

    AMD javascript options.js file
    This is the first pass to get a working ID on the click event

--- mod/comments/amd/src/options.js
+++ mod/comments/amd/src/options.js
@@ -1,10 +1,15 @@
-
 define(['jquery', 'core/ajax', 'core/templates', 'core/notification'], function($, ajax, templates, notification) {
     return {
-        remove: function() {
-            // Add a click handler to the delete button
-            $('[data-region="comment-posts"] #remove').on('click', function () {
-                alert("This post is to be deleted");
+        init: function () {
+            // add click handler to create alert to show message id to be liked
+            $('[data-region="comment-posts"] .like').on('click', function () {
+                var msgID = $(this).data('id');
+                alert("This post is to be liked " + msgID);
+            });
+            // add click handler to create alert to show message id to be deleted
+            $('[data-region="comment-posts"] .delete').on('click', function () {
+                var msgID = $(this).data('id');
+                alert("This post is to be deleted " + msgID);
             });
         }
     };
