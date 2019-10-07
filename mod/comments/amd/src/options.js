047e5da43b1fa71b7c2e1af0fe44beedb82ae38c
Kieran Briggs <kbriggs@kbwebsolutions.co.uk>
2019-10-07 16:45:37 +0100

    Logic and Styles for the Like button

--- mod/comments/amd/src/options.js
+++ mod/comments/amd/src/options.js
@@ -4,13 +4,23 @@ define(['jquery', 'core/ajax', 'core/templates', 'core/notification'], function(
             // add click handler to create alert to show message id to be liked
             $('[data-region="comment-posts"] .like').on('click', function () {
                 var msgID = $(this).data('id');
-                alert("This post is to be liked " + msgID);
+                if ($(this).hasClass('likeoff')) {
+                    $(this).addClass('likeon').removeClass('likeoff');
+                } else {
+                    $(this).removeClass('likeon').addClass('likeoff');
+                }
+                //alert("This post is to be liked " + msgID);
             });
             // add click handler to create alert to show message id to be deleted
             $('[data-region="comment-posts"] .delete').on('click', function () {
                 var msgID = $(this).data('id');
                 alert("This post is to be deleted " + msgID);
             });
+            // add click handler to create alert to show message id to be flag
+            $('[data-region="comment-posts"] .flag').on('click', function () {
+                var msgID = $(this).data('id');
+                notification('This post ( '+msgID+' ) be flagged');
+            });
         }
     };
-});
+})
